2c2a98a9284cc0b76641734afdc48997494daf17
Shweta Shaw <189984154+ShwetaShaw76@users.noreply.github.com>
2025-12-16 20:28:18 +0000

    Making the loading screen

---
+++ src/Screen.vue
@@ -0,0 +1,27 @@
+<script setup></script>
+
+<template>
+    <div class="screen">
+    <h1>Turquoise Os</h1>
+    <div id="loading"></div>
+    </div>
+</template>
+
+<style scoped>
+    .screen {
+        display:flex;
+        background-color: rgba(0, 0, 0, 0.8);
+        height: 100vh;
+        justify-content: center;
+        align-items: center;
+        width:35vw;
+        color:white;
+        text-align:center;
+    }
+    @media screen and (max-width: 600px) {
+        .screen{
+            width: 100vw;
+            text-align:center;
+        }
+    }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import Screen from './Screen.vue'
 import router from './router'
 
 const app = createApp(App)
+const screen = createApp(Screen)
 
 app.use(router)
 
-app.mount('#app')
+// app.mount('#app')
+screen.mount('#app')
