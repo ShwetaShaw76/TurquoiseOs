409051412744159b881c9a078cca2dd3e785b1b6
Shweta Shaw <189984154+ShwetaShaw76@users.noreply.github.com>
2026-02-09 17:03:16 +0000

    made the Ui of the call Screen

--- src/components/Callscreen.vue
+++ src/components/Callscreen.vue
@@ -43,6 +43,7 @@
 
 <template>
     <div class="callScreen">
+        <div class="scnd">
         <header @mousedown="(e)=startDrag(e,'header')"><Header></Header></header>
         <div class="top">
         <div class="name">Bob</div>
@@ -51,30 +52,32 @@
         </div>
         <div class="bottom">
             <div class="btn"></div>
-            <div class="btn"><span class="material-symbols-outlined">
+            <div class="btn"><span class="material-symbols-outlined sym">
 pause
 </span></div>
-            <div class="btn"><span class="material-symbols-outlined">
+            <div class="btn"><span class="material-symbols-outlined sym">
 add
 </span></div>
-            <div class="btn"><span class="material-symbols-outlined">
+            <div class="btn"><span class="material-symbols-outlined sym">
 mic_off
 </span></div>
-            <div class="btn"><span class="material-symbols-outlined">
+            <div class="btn"><span class="material-symbols-outlined sym">
 video_camera_front
 </span></div>
-            <div class="btn"><span class="material-symbols-outlined">
+            <div class="btn"><span class="material-symbols-outlined sym">
 robot_2
 </span></div>
-            <div class="btn"><span class="material-symbols-outlined">
+            <div class="btn"><span class="material-symbols-outlined sym">
 volume_up
 </span></div>
-            <div class="btn"><span class="material-symbols-outlined">
+            <div class="btn"><span class="material-symbols-outlined sym">
 call_end
-</span><span class="material-symbols-outlined">
+</span></div>
+            <div class="btn"><span class="material-symbols-outlined sym">
 dialpad
 </span></div>
         </div>
+    </div>
     </div>
 </template>
 
@@ -84,5 +87,57 @@ dialpad
     aspect-ratio:9/16;
     display:flex;
     flex-direction:column;
+    background:url("/assets/flower_background.jpg");
+    background-size:contain;
+    background-position:center;
+    background-repeat:no-repeat;
+    position:relative;
+}
+.scnd{
+    content:"";
+    backdrop-filter:blur(2px);
+    inset:0;
+    background: rgba(0, 0, 0, 0.4);
+    position:absolute;
+    z-index:1;
+}
+.top{
+    height:40vh;
+    width:100%;
+    display:flex;
+    justify-content:center;
+    flex-direction:column;
+    align-items:center;
+    color:white;
+}
+.name{
+    font-size:3rem;
+    font-weight:700;
+}
+.call{
+    padding:20px;
+    font-size:1rem;
+    font-weight:500;
+}
+.btn{
+    height:15vh;
+    width:15vh;
+    border:1px solid grey;
+    border-radius:50%;
+    display:flex;
+    align-items:center;
+    justify-content:center;
+    color:white;
+    box-shadow: 0px 0px 20px 0.7px rgba(255, 255, 255, 0.5) inset;
+}
+.bottom{
+    padding:17px;
+    display: grid;
+  grid-template-rows: repeat(3, 3fr);
+  grid-template-columns: repeat(3, 3fr);
+  gap:15px;
+}
+.sym{
+    font-size:3rem;
 }
 </style>
